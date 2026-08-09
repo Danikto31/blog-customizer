0b291ee30b86ce49c1f898dd5ba756a73905089e
Arseniy <huycuka9@gmail.com>
2026-08-09 15:21:55 +0300

    feat: Added functionality to the form based on the specifications and layout in Figma

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,99 @@
-import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
-
+import { useState } from 'react';
+import { ArrowButton } from '../../ui/arrow-button';
+import { Button } from '../../ui/button';
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
+import { Text } from '../../ui/text';
+import { Select } from 'src/ui/select';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import { RadioGroup } from 'src/ui/radio-group';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	setFormState: React.Dispatch<React.SetStateAction<ArticleStateType>>;
+};
+
+export const ArticleParamsForm = ({ setFormState }: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [localState, setLocalState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	function handleSubmit(e: React.FormEvent) {
+		e.preventDefault();
+		setFormState(localState);
+	}
+
+	function onChange(field: keyof ArticleStateType, option: OptionType) {
+		setLocalState((obj) => ({
+			...obj,
+			[field]: option,
+		}));
+	}
+
+	const handleReset = () => {
+		setLocalState(defaultArticleState);
+		setFormState(defaultArticleState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text uppercase weight={800} size={31} family='open-sans'>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={localState.fontFamilyOption}
+						onChange={(option) =>
+							onChange('fontFamilyOption', option)
+						}></Select>
+					<RadioGroup
+						name='fontRadio'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={localState.fontSizeOption}
+						onChange={(option) =>
+							onChange('fontSizeOption', option)
+						}></RadioGroup>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={localState.fontColor}
+						onChange={(option) => onChange('fontColor', option)}></Select>
+					<div className={styles.fontColorContainer}>
+						<Select
+							title='Цвет фона'
+							options={backgroundColors}
+							selected={localState.backgroundColor}
+							onChange={(option) =>
+								onChange('backgroundColor', option)
+							}></Select>
+					</div>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={localState.contentWidth}
+						onChange={(option) => onChange('contentWidth', option)}></Select>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
